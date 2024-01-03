d855e4016ed877ab92591b16e6b815d8f322c7dc
Trevor Grant <trevor.d.grant@gmail.com>
2024-01-02 22:45:46 -0600

    Closes DD-62 and DD-63

--- src/components/ChatBubble/ChatBubble.js
+++ src/components/ChatBubble/ChatBubble.js
@@ -1,30 +1,55 @@
 import React, { useState } from 'react';
 import ReactMarkdown from 'react-markdown';
-import { FaFile, FaComment } from 'react-icons/fa';
+import { FaFile, FaComment, FaInfoCircle } from 'react-icons/fa';
 
 function ChatBubble({ message }) {
     const [showSources, setShowSources] = useState(false);
+    const [showMetadata, setShowMetadata] = useState(false);
     const hasSources = message.sources && message.sources.length > 0;
+    const hasMetadata = message.metadata;
 
+    const handleToggleText = () => {
+        setShowSources(false);
+        setShowMetadata(false);
+    }
     const handleToggleSources = () => {
         if (hasSources) {
             setShowSources(!showSources);
+            setShowMetadata(false);
         } else {
             console.log("No sources to show, ignoring.")
         }
 
     };
 
+    const handleToggleMetadata = () => {
+        if (hasMetadata) {
+            setShowMetadata(!showMetadata);
+            setShowSources(false);
+        } else {
+            console.log("No metadata to show, ignoring.")
+        }
+    };
+
     return (
         <div className={`message ${message.isUser ? "user" : "ai"}`}>
             <div className="message-content">
-                {showSources ? (
+                {showSources && !showMetadata ? (
                     <div>
                         {message.sources.map((source, index) => (
                             <div key={index}>
                                 <a href={source.url} target="_blank" rel="noopener noreferrer">
                                     {source.title !== "" ? source.title : source.url}
                                 </a>
+                            </div>
+                        ))}
+                    </div>
+                ) : showMetadata ? (
+                    <div>
+                        {Object.entries(message.metadata).map(([key, value]) => (
+                            <div key={key}>
+                                <span>{key}: </span>
+                                <span>{value}</span>
                             </div>
                         ))}
                     </div>
@@ -35,13 +60,18 @@ function ChatBubble({ message }) {
             <div className="message-icons">
                 <FaComment
                     className={`message-icon ${showSources ? "active" : ""}`}
-                    onClick={handleToggleSources}
+                    onClick={handleToggleText}
                 />
                 {hasSources && (
                     <FaFile
                         className={`message-icon ${showSources ? "" : "active"}`}
                         onClick={handleToggleSources}
                     />
+                )}
+                {hasMetadata && (<FaInfoCircle
+                    className={`message-icon ${showMetadata ? "active" : ""}`}
+                    onClick={handleToggleMetadata}
+                />
                 )}
             </div>
         </div>
